53400225d6e90714fe22b70dbe2d03ccba9b1276
Erick Mucira <muciraerick254@gmail.com>
2024-05-03 15:16:56 +0300

    update: responsiveness added

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -6,19 +6,25 @@ import FavTwo from "@/assets/favtwo.png";
 const Favorite = () => {
   return (
     <section className="mx-auto mb-10 w-10/12">
-      <h2 className="text-3xl font-extrabold uppercase">Young's Favourite</h2>
+      <h2 className="text-2xl font-extrabold uppercase md:text-3xl">
+        Young's Favourite
+      </h2>
       <div className="relative">
         <img
           src={Mark}
           alt="marker"
-          className="absolute -top-4 left-52 -z-20 h-5 -rotate-3"
+          className="absolute -top-4 left-36 -z-20 h-5 -rotate-3 md:left-52"
         />
       </div>
 
-      <div className="my-12 grid h-[475px] grid-cols-2">
-        <div className="">
-          <img src={FavOne} alt="image" className=" h-[400px]" />
-          <div className="mb-1 mt-4 flex w-[575px] items-center justify-between gap-8 font-medium opacity-75">
+      <div className="my-12 grid h-auto grid-cols-1 space-y-8 md:h-[475px] md:grid-cols-2 md:space-y-0">
+        <div>
+          <img
+            src={FavOne}
+            alt="image"
+            className="h-[400px] rounded-lg object-cover object-[24%] md:object-center"
+          />
+          <div className="mb-1 mt-4 flex w-full items-center justify-between gap-8 font-medium opacity-75 md:w-[575px]">
             <h2>Trending on instagram</h2>
             <BsArrowRight className=" text-2xl opacity-60" />
           </div>
@@ -30,8 +36,12 @@ const Favorite = () => {
           </a>
         </div>
         <div className="">
-          <img src={FavTwo} alt="image" className=" h-[400px]" />
-          <div className="mb-1 mt-4 flex w-[575px] items-center justify-between gap-8 font-medium opacity-75">
+          <img
+            src={FavTwo}
+            alt="image"
+            className=" h-[400px] rounded-lg object-cover object-center md:object-center"
+          />
+          <div className="mb-1 mt-4 flex w-full items-center justify-between gap-8 font-medium opacity-75 md:w-[575px]">
             <h2>All under $40</h2>
             <BsArrowRight className=" text-2xl opacity-60" />
           </div>
